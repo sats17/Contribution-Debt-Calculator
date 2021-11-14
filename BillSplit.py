675bfb631bbc46b4f3461ad955ddbc0876c9b277
sats17 <satishkumbhar9999@gmail.com>
2021-11-14 14:40:22 +0530

    Added basic structure of html

--- BillSplit.py
+++ BillSplit.py
@@ -248,15 +248,15 @@ def main():
 
 
 if __name__ == "__main__":
-    expense = {'food': {'bill': 250, 'payments': {'1': 20, '2': 32, '3': 43, '4': 35, '5': 65, '6': 12, '7': 43}},
-               'rent': {'bill': 2492, 'payments': {'1': 902, '2': 98, '3': 84, '4': 35, '5': 32, '6': 552, '7': 789}},
-               'vadapav': {'bill': 70, 'payments': {'1': 10, '2': 10, '3': 10, '4': 10, '5': 10, '6': 10, '7': 10}},
-               'samosa': {'bill': 110, 'payments': {'1': 15, '2': 15, '3': 5, '4': 20, '5': 15, '6': 25, '7': 15}},
-               'pizza': {'bill': 1700,
-                         'payments': {'1': 50, '2': 150, '3': 200, '4': 250, '5': 300, '6': 350, '7': 400}}}
-    # print(calculateTotalPaymentByEachMembers(expense))
-    events = getPaymentExchangeInformation(7, expense)
-    print("So we have resolved your contributions headache, please check below..")
-    for event in events:
-        print(event)
-    # main()
+    # expense = {'food': {'bill': 250, 'payments': {'1': 20, '2': 32, '3': 43, '4': 35, '5': 65, '6': 12, '7': 43}},
+    #            'rent': {'bill': 2492, 'payments': {'1': 902, '2': 98, '3': 84, '4': 35, '5': 32, '6': 552, '7': 789}},
+    #            'vadapav': {'bill': 70, 'payments': {'1': 10, '2': 10, '3': 10, '4': 10, '5': 10, '6': 10, '7': 10}},
+    #            'samosa': {'bill': 110, 'payments': {'1': 15, '2': 15, '3': 5, '4': 20, '5': 15, '6': 25, '7': 15}},
+    #            'pizza': {'bill': 1700,
+    #                      'payments': {'1': 50, '2': 150, '3': 200, '4': 250, '5': 300, '6': 350, '7': 400}}}
+    # # print(calculateTotalPaymentByEachMembers(expense))
+    # events = getPaymentExchangeInformation(7, expense)
+    # print("So we have resolved your contributions headache, please check below..")
+    # for event in events:
+    #     print(event)
+    main()
--- assistance.js
+++ assistance.js
@@ -191,8 +191,13 @@ function eventGenerator(creditorName, amount, debtorName){
                'vadapav': {'bill': 70, 'payments': {'1': 10, '2': 10, '3': 10, '4': 10, '5': 10, '6': 10, '7': 10}},
                'samosa': {'bill': 110, 'payments': {'1': 15, '2': 15, '3': 5, '4': 20, '5': 15, '6': 25, '7': 15}},
                'pizza': {'bill': 1700, 'payments': {'1': 50, '2': 150, '3': 200, '4': 250, '5': 300, '6': 350, '7': 400}}}
+expense =  {'food': {'bill': 250, 'payments': {'1': 20, '2': 32, '3': 43, '4': 35, '5': 65, '6': 12, '7': 43}},
+               'rent': {'bill': 2492, 'payments': {'1': 902, '2': 98, '3': 84, '4': 35, '5': 32, '6': 552, '7': 789}},
+               'vadapav': {'bill': 70, 'payments': {'1': 10, '2': 10, '3': 10, '4': 10, '5': 10, '6': 10, '7': 10}},
+               'samosa': {'bill': 110, 'payments': {'1': 15, '2': 15, '3': 5, '4': 20, '5': 15, '6': 25, '7': 15}},
+               'pizza': {'bill': 1700, 'payments': {'1': 50, '2': 150, '3': 200, '4': 250, '5': 300, '6': 350, '7': 400}}}
 
 //let a = zipPaymentAndNames(['a', 'b', 'c'], ['12', '14', '33','12'])
 //differentiateCreditorAndDebtor(a, 14)
-events = getDebtInformation(7, expense)
-console.log(events)
+// events = getDebtInformation(7, expense)
+// console.log(events)
